04b26626cd1a969b6d0446840fc00074cc56c8ab
csharpstudy <csharpstudy@live.com>
2013-05-13 23:55:50 -0700

    Lab2

---
+++ Lab2_콘솔프로그램/Solution/PerfTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace PerfTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 스톱워치 객체 생성 (Stopwatch)
+            Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+
+            // 스톱워치 측정 시작
+            stopwatch.Start();
+
+            // 본 작업 실행
+            Console.WriteLine("Task Running...");
+            int result = Calc();
+
+            // 스톱워치 측정 중지
+            stopwatch.Stop();
+
+            // 결과 출력
+            Console.WriteLine("Result: {0}", result);            
+
+            // 측정시간 출력
+            Console.WriteLine("Elapsed Time: {0}", stopwatch.Elapsed);
+        }
+
+        static int Calc()
+        {
+            int sum = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                sum += i;
+                Thread.Sleep(100);                
+            }
+            return sum;
+        }
+    }
+}
